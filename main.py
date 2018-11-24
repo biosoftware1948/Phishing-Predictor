539419cfa3af93e820c8a7aca14fa7b03c1ce6a2
Kevin Velcich <kvelcich@gmail.com>
2018-11-24 02:00:39 -0800

    Adding other classifiers

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ def label_occurences(data):
     for val in data:
         if val not in label_dict:
             label_dict[val] = 1
-        else:
-            label_dict[val]+=1
+        else: 
+            label_dict[val] += 1
     
     for key, val in label_dict.items():
-        print("value " + str(key) + " occurs " + str(val/len(data)) + " percent in dataset")
+        print("value " + str(key) + " occurs " + str(100.0*val/len(data)) + " percent in dataset")
 
 def main():
     dataSet = PhishingData(FILE_NAME)
@@ -19,10 +19,23 @@ def main():
 
     label_occurences(dataSet.y_test)
 
-    from sklearn.ensemble import RandomForestClassifier
-    clf = RandomForestClassifier(n_estimators=100, max_depth=2, random_state=0)
+    classifier = input("\nChoose a classifier:\n1. Random Forest\n2. Linear SVC\n3. K Neighbors\n");
+    if classifier == 1:
+        from sklearn.ensemble import RandomForestClassifier
+        clf = RandomForestClassifier(n_estimators=100, max_depth=2, random_state=0)
+    elif classifier == 2:
+        from sklearn.svm import LinearSVC
+        clf = LinearSVC(random_state=0, tol=1e-6)
+    elif classifier == 3:
+        from sklearn.neighbors import KNeighborsClassifier 
+        clf = KNeighborsClassifier(n_neighbors=12)
+    else:
+        return
+
     clf.fit(dataSet.x_train, dataSet.y_train)
     y_prediction = clf.predict(dataSet.x_test)
+
+    
     from sklearn.metrics import accuracy_score
     print ("Accuracy of classifier: " + str((accuracy_score(dataSet.y_test, y_prediction))))
 
